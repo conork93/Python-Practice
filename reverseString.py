dfa39a2fa5f995c3e0959af8eace84fdda212c05
Conor Kennedy <kennedyconor93@gmail.com>
2017-07-21 13:10:41 -0500

    Create reverseString.py

---
+++ reverseString.py
@@ -0,0 +1,15 @@
+'''
+Conor Kennedy
+7/21/17
+Write a function that takes a string as input
+and returns the string reversed.
+'''
+
+s = 'conor kennedy'
+
+def reverseString(x):
+  return x[::-1]
+   
+ans = reverseString(s)
+
+print (ans)

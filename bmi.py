eb1a60b4fa41e21ee98d07c0522d501cb3588ba7
Conor Kennedy <kennedyconor93@gmail.com>
2017-08-13 22:53:08 -0500

    metric bmi calc

---
+++ bmi.py
@@ -0,0 +1,11 @@
+#Conor Kennedy
+#8/13/2017
+
+#calculate bmi 
+
+height = input("Enter height in meters: ")
+weight = input("Enter weight in kg: ")
+
+bmi = (weight)/(height * height)
+
+print("BMI: ")+("%.1f" % bmi)

9c2d4f98d9f82f53257f645681aba8b485ed349e
Conor Kennedy <kennedyconor93@gmail.com>
2017-08-16 23:05:50 -0500

    add a couple functions not done

--- bmi.py
+++ bmi.py
@@ -9,3 +9,18 @@ weight = input("Enter weight in kg: ")
 bmi = (weight)/(height * height)
 
 print("BMI: ")+("%.1f" % bmi)
+
+
+#calculate bmi with pounds and inches
+def mericaUnits(height, weight){
+  return (weight/(height * height)) * 703
+}
+
+def feetToInches(feet){
+  return feet * 12
+}
+
+#calculate bmi with meters and kg
+def restOfTheWorld(height, weight){
+  return (weight)/(height * height)
+}
